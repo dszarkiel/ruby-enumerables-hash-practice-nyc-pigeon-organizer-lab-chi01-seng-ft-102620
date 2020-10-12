b29689f7c1fcb2c4b93b29cbb89cb6f778e2b96c
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-12 17:17:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
     #binding.pry
     value.each do |inner_key, names|
       names.each do |name|
-        binding.pry
+        #binding.pry
         if !final_array[name]
           final_array[name] = {}
         end
@@ -11,5 +11,5 @@ def nyc_pigeon_organizer(data)
     end
     final_array
   end
-  binding.pry
+  #binding.pry
 end
